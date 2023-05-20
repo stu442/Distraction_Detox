b0e81fc90c56ceefe61c827f121a27a310e00e35
stu442 <stu442@naver.com>
2023-05-20 23:25:19 +0900

    feat : 타이머 기능 추가
    new Date() 를 활용한 타이머

--- timer.js
+++ timer.js
@@ -1,35 +1,54 @@
+const startBtn = document.getElementById('start');
 let timerInterval;
-let seconds = 0;
 
 function startTimer() {
-document.getElementById('start').innerText = 'Stop';
-timerInterval = setInterval(updateTimer, 1000);
+  startBtn.innerText = "Stop"
+  // 스토리지에 데이터 저장
+  chrome.storage.sync.set({ 'startTime' : new Date().getTime() });
+  chrome.storage.sync.set({ 'isTiming' : true });
+  timerInterval = setInterval(updateTimer, 1000);
 }
 
 function stopTimer() {
-clearInterval(timerInterval);
-timerInterval = 0
-document.getElementById('start').innerText = 'Start';
-seconds = 0;
-updateTimer(true);
+  startBtn.innerText = "Start"
+  chrome.storage.sync.set({ 'isTiming' : false });
+  clearInterval(timerInterval);
+  timerInterval = 0;
+  document.getElementById('timer').innerText = "00:00"
 }
 
-function updateTimer(isStop) {
-!isStop ? seconds++ : null;
-const minutes = Math.floor(seconds / 60);
-const remainingSeconds = seconds % 60;
-const formattedTime = `${padNumber(minutes)}:${padNumber(remainingSeconds)}`;
-document.getElementById('timer').innerText = formattedTime;
+function updateTimer() {
+  chrome.storage.sync.get('startTime', (result) => {
+    let seconds = Math.floor((new Date().getTime() - result.startTime)/1000)
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    const formattedTime = `${padNumber(minutes)}:${padNumber(remainingSeconds)}`;
+    document.getElementById('timer').innerText = formattedTime;
+  })
 }
 
 function padNumber(number) {
-return number.toString().padStart(2, '0');
+  return number.toString().padStart(2, '0');
 }
 
-document.getElementById('start').addEventListener('click', function() {
-if (timerInterval) {
-  stopTimer();
-} else {
-  startTimer();
+startBtn.addEventListener('click', () => {
+  if(startBtn.innerText === "Start") {
+    startTimer();
+  } else {
+    stopTimer();
+  }
+})
+
+function isPopupOpen() {
+  const views = chrome.extension.getViews({ type: 'popup' });
+  return views.length > 0;
 }
-});
+
+if (isPopupOpen()) {
+  chrome.storage.sync.get('isTiming', (result) => {
+    if(result.isTiming) {
+      startBtn.innerText = "Stop"
+      timerInterval = setInterval(updateTimer, 1000);
+    }
+  })
+}
